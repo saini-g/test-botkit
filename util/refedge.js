37bd73172a6ce15ab16328d5e54bdb46ed6c800a
saini-g <saini.gaurav2205@gmail.com>
2019-08-07 16:57:04 +0530

    removed namespace from sf API endpoint

--- util/refedge.js
+++ util/refedge.js
@@ -2,7 +2,7 @@ const logger = require('../util/logger');
 
 module.exports = {
     saveTeamId: (conn, teamData) => {
-        conn.apex.post('/refedge/rebot', teamData, (err, res) => {
+        conn.apex.post('/rebot', teamData, (err, res) => {
 
             if (err) {
                 logger.log(err);
